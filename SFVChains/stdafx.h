95153aa8be1795a170070502b08414054f933411
Bryan O'Malley <bo122081@fullsail.edu>
2016-10-20 20:33:14 -0400

    Added updated tinyxml2 files for VS2015

--- SFVChains/stdafx.h
+++ SFVChains/stdafx.h
@@ -21,10 +21,10 @@
 #include <concurrent_vector.h>
 #include <atomic>
 #include <tinyxml2.h>
+#include <ppl.h>
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
 #include <Windows.h>
-#include <ppl.h>
 
 #define MIN(a,b) (((a) < (b))?(a):(b))
 #define MAX(a,b) (((a) > (b))?(a):(b))
